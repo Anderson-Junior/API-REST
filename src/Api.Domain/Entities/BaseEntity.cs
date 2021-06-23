c46ae67a65e17f455fcc02af45d7e34de8ca6dde
Anderson-Junior <ander.lemos.jr@gmail.com>
2021-06-23 20:43:08 -0300

    Implementing BaseEntity and UserEntity

---
+++ src/Api.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+namespace Api.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        private DateTime? _createdAT { get; set; }
+        public DateTime? CreatedAt
+        {
+            get { return _createdAT; }
+            set { _createdAT = (value == null ? DateTime.UtcNow : value); }
+        }
+        public DateTime? UpdateAt { get; set; }
+    }
+}
